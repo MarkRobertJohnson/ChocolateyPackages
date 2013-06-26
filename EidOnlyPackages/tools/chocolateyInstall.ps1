48ce06cb42477aa9028ede3b024a75033ec66fa7
Mark R. Johnson <mjohnson@eidpassport.com>
2013-06-26 11:32:22 -0700

    Added NetCFPowerToys

---
+++ EidOnlyPackages/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿   Install-ChocolateyPackage -packageName "$packageName" `
+   						-fileType 'msi' `
+   						-url 'http://download.microsoft.com/download/f/a/c/fac1342d-044d-4d88-ae97-d278ef697064/NETCFv35PowerToys.msi' `
+   						-silentArgs '/q /norestart'
