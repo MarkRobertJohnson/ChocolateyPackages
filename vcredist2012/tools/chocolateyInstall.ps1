fd6cec370881f46b5a652eba5794b5ec56126bee
unknown <mjohnson@DEV-MJOHNSON.eidpassport.com>
2013-10-01 09:45:29 -0700

    Added vcredist2012 package

---
+++ vcredist2012/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿Install-ChocolateyPackage -packageName $packageName -fileType 'exe' -silentArgs '/Q' `
+                    -url 'http://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU3/vcredist_x86.exe' `
+                    -url64bit 'http://download.microsoft.com/download/1/6/B/16B06F60-3B20-4FF2-B699-5E9B7962F9AE/VSU3/vcredist_x64.exe'
+
+
