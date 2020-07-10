9dea0b2852b9fff4c5a203ac810bdfac8afbbe15
Michal Kovac <mk185147@ncr.com>
2020-07-10 17:03:05 +0200

    Update vcredist2010 package to newer version to 10.0.40219.325

--- vcredist2010/tools/chocolateyInstall.ps1
+++ vcredist2010/tools/chocolateyInstall.ps1
@@ -1,14 +1,12 @@
-﻿try { 
-  $processor = Get-WmiObject Win32_Processor
-  $is64bit = $processor.AddressWidth -eq 64
-  Install-ChocolateyPackage 'vcredist2010' 'exe' '/Q' 'http://download.microsoft.com/download/C/6/D/C6D0FD4E-9E53-4897-9B91-836EBA2AACD3/vcredist_x86.exe'
-  if($is64bit) {
-  	Install-ChocolateyPackage 'vcredist2010_x64' 'exe' '/Q' 'http://download.microsoft.com/download/A/8/0/A80747C3-41BD-45DF-B505-E9710D2744E0/vcredist_x64.exe'
-  }
+﻿$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
+Install-ChocolateyPackage 'vcredist2010' 'exe' '/Q' 'https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe' `
+            -checksum 67313B3D1BC86E83091E8DE22981F14968F1A7FB12EB7AD467754C40CD94CC3D `
+            -checksumType sha256
 
-  # the following is all part of error handling
-  Write-ChocolateySuccess 'vcredist2010'
-} catch {
-  Write-ChocolateyFailure 'vcredist2010' "$($_.Exception.Message)"
-  throw 
+                
+if($is64bit) {
+    Install-ChocolateyPackage 'vcredist2010_x64' 'exe' '/Q' 'https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x64.exe' `
+            -checksum64 CC7EC044218C72A9A15FCA2363BAED8FC51095EE3B2A7593476771F9EBA3D223 `
+            -checksumType64 sha256
 }
